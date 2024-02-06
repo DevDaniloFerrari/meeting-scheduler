7bb69f1d9c6a42ec7dd5899bc4723e1bd020a192
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-06 19:12:01 -0300

    utilizando conceito SSR

---
+++ app/estatico3/page.jsx
@@ -0,0 +1,19 @@
+async function obterRandomNumber() {
+  let random;
+    await fetch("https://...", { next: { revalidate: 7 } })
+    .catch(() => {})
+    .finally(() => {
+      random = Math.random();
+    });
+  return random;
+}
+
+export default async function Estatico3() {
+  const numero = await obterRandomNumber();
+  return (
+    <div>
+      <h1>Estático #02</h1>
+      <h2>{numero}</h2>
+    </div>
+  );
+}
