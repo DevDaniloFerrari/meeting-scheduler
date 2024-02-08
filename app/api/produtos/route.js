5cff793d27d4a7e92bddbe2ab555d43cc7e7ebe0
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-08 07:19:56 -0300

    implementando SSR com api

--- app/api/produtos/route.js
+++ app/api/produtos/route.js
@@ -1,10 +1,14 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+
+function numeroAleatorio(min = 1, max = 100000) {
+  return parseInt(Math.random() * (max - min) + min);
+}
 
 export async function GET() {
   return NextResponse.json([
-    {id: 1, nome: 'Caneta', preco: 5.60},
-    {id: 2, nome: 'Caderno', preco: 15.60},
-    {id: 3, nome: 'Borracha', preco: 7.30},
-    {id: 4, nome: 'Tesoura', preco: 21.55},
+    { id: numeroAleatorio(), nome: "Caneta", preco: 5.6 },
+    { id: numeroAleatorio(), nome: "Caderno", preco: 15.6 },
+    { id: numeroAleatorio(), nome: "Borracha", preco: 7.3 },
+    { id: numeroAleatorio(), nome: "Tesoura", preco: 21.55 },
   ]);
 }
--- app/dinamico2/page.jsx
+++ app/dinamico2/page.jsx
@@ -1,15 +1,16 @@
 async function obterProdutos() {
-  const response = await fetch("http://localhost:3000/api/produtos");
-  const produtos = await response.json();
-  return produtos;
+  console.log('[Server] gerando dados no servidor')
+  const response = await fetch("http://localhost:3000/api/produtos", {
+    cache: "no-store",
+  });
+  return response.json();
 }
 
-export default async function Estatico4() {
+export default async function Dinamico2() {
   const produtos = await obterProdutos();
-
   return (
     <div>
-      <h1>Estático #04</h1>
+      <h1>Dinâmico #02</h1>
       <ul>
         {produtos.map((produto) => (
           <li key={produto.id}>
