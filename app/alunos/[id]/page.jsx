c809f51bba9b4ce0281de4edc2e6994aa270195f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-08 07:51:37 -0300

    obtendo dados da api com static paths

--- app/alunos/[id]/page.jsx
+++ app/alunos/[id]/page.jsx
@@ -9,10 +9,23 @@ export function getStaticPaths() {
   };
 }
 
-export default function AlunoPorId() {
+async function obterAlunoPorId(id) {
+  const response = await fetch(`http://localhost:3000/api/alunos/${id}`);
+  const alunos = await response.json();
+  return alunos[0]
+}
+
+export default async function AlunoPorId(props) {
+  const id = props.params.id;
+  const aluno = await obterAlunoPorId(id);
   return (
     <div>
       <h1>Detalhes do Aluno</h1>
+      <ul>
+        <li>{aluno.id}</li>
+        <li>{aluno.nome}</li>
+        <li>{aluno.email}</li>
+      </ul>
     </div>
   );
 }
